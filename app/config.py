3d858c38299b74de68b07bde203624303d124c76
Porto <rodrigoandradeporto@hotmail.com>
2021-12-03 15:28:52 -0300

    refactor: update app config

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-app.config['BASIC_AUTH_USERNAME']= os.environ['BASIC_AUTH_USERNAME']
-app.config['BASIC_AUTH_PASSWORD']= os.environ['BASIC_AUTH_PASSWORD']
+app.config['BASIC_AUTH_USERNAME'] = os.environ['BASIC_AUTH_USERNAME']
+app.config['BASIC_AUTH_PASSWORD'] = os.environ['BASIC_AUTH_PASSWORD']
 
 auth = BasicAuth(app)
 
@@ -16,10 +16,9 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
 app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB_CLIENTES']
-app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB_PRODUTOS']
-app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB_VENDAS']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB_SERVICOS']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB_PEDIDOS']
 app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
 
 
 mysql.init_app(app)
-
